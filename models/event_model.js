f078677dcbf4c7f2e2ab54015c19fa134a6373d8
rastringer <ra1stringer@gmail.com>
2015-12-17 12:04:46 -0800

    adding foundation

---
+++ models/event_model.js
@@ -0,0 +1,21 @@
+// var mongoose = require('mongoose');
+
+// //creating event's schema
+// var eventSchema = new mongoose.Schema({
+//   name: String,
+//   description: String,
+//   time: Date,
+//   date: Date,
+//   address: String,
+//   url: String,
+//   location: {
+//     city: String,
+//     state: String,
+//     zip: Number
+//   },
+//   company_name: String
+// });
+
+// var Event = mongoose.model('Event', eventSchema);
+// module.exports = Event;
+
---
+++ nlp-insights.js
@@ -0,0 +1,67 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+ Meteor.methods({
+   eventbriteDataGet: function(){
+     this.unblock();
+     return Meteor.http.call("GET", "https://www.eventbriteapi.com/v3/events/search/?q=chilling&location.latitude=47.6097&location.longitude=122.3331&location.within=10km&popular=true&token=MO5AQ24HAYLNBP7L5WLE");
+    },
+
+    eventfulDataGet: function(){
+     this.unblock();
+     return Meteor.http.call("GET", "http://api.eventful.com/json/events/search?c=music&l=Seattle&app_key=C5VJScp667pVNMHB&keywords=childish+gambino");
+   },
+
+    meetupDataGet: function(){
+      this.unblock();
+      return Meteor.http.call("GET", "https://api.meetup.com/2/open_events.json?zip=98109&time=,2m&key=595675274d4211175b522771323d075");
+    },
+
+    brownPaperTicketDataGet: function(){
+     this.unblock();
+     return Meteor.http.call("GET", "https://www.brownpapertickets.com/api2/eventlist/?id=KxsUrh2jzn");
+    }
+
+ });
+
+ Meteor.startup(function () {
+    // Meteor.call("eventbriteDataGet", function(error, result){
+    //   if(error) console.log("The error is " + error);
+    //   console.log(result.content);
+    // });
+    //
+    // Meteor.call("eventfulDataGet", function(error, result){
+    //   if(error) console.log("The error is " + error);
+    //   console.log(result.body);
+    // });
+    //
+    // Meteor.call("meetupDataGet", function(error, result){
+    //   if(error) console.log("The error is " + error);
+    //   console.log("server stuff");
+    //   result = JSON.parse(result.content);
+    //   console.log(result.results[0]);
+    // });
+
+   //  Meteor.call("brownPaperTicketDataGet", function(error, result){
+   //   if(error) console.log("The error is " + error);
+   //   console.log(result);
+   // });
+
+ });
+}
