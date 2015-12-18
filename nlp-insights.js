affd4f471f37b06f98820fdedd90f8791a1938f7
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-12-18 11:16:49 -0800

    Formatting the data and time for eventful api

--- nlp-insights.js
+++ nlp-insights.js
@@ -80,16 +80,16 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
  Meteor.methods({
-  /*eventbriteDataGet: function(){
+/*  eventbriteDataGet: function(){
    this.unblock();
    return Meteor.http.call("GET", "https://www.eventbriteapi.com/v3/events/search/?q=music&location.address=Seattle&token=MO5AQ24HAYLNBP7L5WLE");
   }*/
   //don't forget to put comma after each method
 
-  eventfulDataGet: function(){
+/*  eventfulDataGet: function(){
    this.unblock();
-   return Meteor.http.call("GET", "http://api.eventful.com/json/events/search?l=Seattle&app_key=C5VJScp667pVNMHB&keywords=story+time+evening+music");
-  }
+   return Meteor.http.call("GET", "http://api.eventful.com/json/events/search?l=Seattle&app_key=C5VJScp667pVNMHB");
+  }*/
 
   /*meetupDataGet: function(){
     this.unblock();
@@ -102,17 +102,33 @@ if (Meteor.isServer) {
  });
 
  Meteor.startup(function () {
-    Meteor.call("eventbriteDataGet", function(error, result){
+   /* Meteor.call("eventbriteDataGet", function(error, result){
       if(error) console.log("The error is " + error)
       var events = JSON.parse(result.content);
       var eventData = events.events;
       for(var i = 0; i < eventData.length -1; i++){
         if(eventData[i].url != undefined)
+          var dateTime = new Date(eventData[i].start.local);
+          var day = dateTime.getDate();
+          var month = dateTime.getMonth() + 1;
+          var year = dateTime.getFullYear();
+          var hour = dateTime.getHours();
+          var minute = dateTime.getMinutes();
+          console.log(month);
+          var dates = day + " "+ month + " " + year;
+          var minuteBuilder = function(minute){
+            if (minute == 0) minute = "00";
+            return  minute;
+          }
+          var minutes = minuteBuilder(minute);
+          var times = hour + ":" + minutes;
+
         Events.insert({
           name: eventData[i].name.text,
           description: eventData[i].description.text,
           address: "Seattle",
-          time: eventData[i].start.local,
+          time: times,
+          date: dates,
           url: eventData[i].url,
           city: "Seattle",
           state: "WA",
@@ -121,8 +137,8 @@ if (Meteor.isServer) {
         });
       };
     });
-    
-   /*Meteor.call("eventfulDataGet", function(error, result){
+    */
+/*   Meteor.call("eventfulDataGet", function(error, result){
       if(error) console.log("The error is " + error);
       var events = JSON.parse(result.content);
       var eventData = events.events.event;
@@ -130,11 +146,10 @@ if (Meteor.isServer) {
         if(eventData[i].url != undefined)
           var dateTime = new Date(eventData[i].start_time);
           var day = dateTime.getDate();
-          var month = dateTime.getMonth();
+          var month = dateTime.getMonth() +1;
           var year = dateTime.getFullYear();
           var hour = dateTime.getHours();
           var minute = dateTime.getMinutes();
-    
           var dates = day+ " "+ month + " " + year;
           var minuteBuilder = function(minute){
             if (minute == 0) minute = "00";
@@ -148,13 +163,13 @@ if (Meteor.isServer) {
           address: eventData[i].venue_address,
           date: dates,
           time: times,
-          url: eventData[i].venue_url,
+          url: eventData[i]["url"],
           city: eventData[i].city_name,
           state: eventData[i].region_abbr,
           zip: eventData[i].postal_code,
           company_name: "Eventful"
-        });
-      };
+        })
+      }
     })*/
     
     /*Meteor.call("meetupDataGet", function(error, result){
@@ -179,10 +194,10 @@ if (Meteor.isServer) {
       }
     });*/
 
-    Meteor.call("brownPaperTicketDataGet", function(error, result){
+    /*Meteor.call("brownPaperTicketDataGet", function(error, result){
      if(error) console.log("The error is " + error);
      console.log(result);
-   });
+   });*/
 
  });
 }
