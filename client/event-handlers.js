1e8580ce77fcfcea941a371fc98aa006c95f9fec
DanLombardy <lombardy.dan@gmail.com>
2015-12-21 19:25:55 -0800

    Add helpers and publish/subsribe to query the backend from front

--- client/event-handlers.js
+++ client/event-handlers.js
@@ -1,15 +1,44 @@
+searchText = "";
+userCategories=[];
+dateInfo = "";
+
 Template.home.events({
     "submit .search-zip": function (event) {
        //Prevent default browser form submit
       event.preventDefault();
+      searchText = event.target.text.value;
+      console.log("Search text is " + searchText);
+      userCategories = categoryBuild(searchText);
+      console.log(userCategories);
+      $(".date-form").css('visibility', 'visible');
 
-      // Get value from form element
-      var zipNum = event.target.text.value;
-      console.log(zipNum);
-
-      Meteor.subscribe("eventData", zipNum);
-
-      // Clear form
-      event.target.text.value = "";
     }
   });
+
+Template.home.events({
+  "submit .date-pick-input": function (event){
+    event.preventDefault();
+    dateInfo = $('#test').val();
+    console.log(dateInfo);
+
+    Meteor.subscribe('eventData', userCategories, dateInfo);
+
+  }
+});
+
+
+// Template.home.events({
+//     "submit .search-zip": function (event) {
+//        //Prevent default browser form submit
+//       event.preventDefault();
+//
+//       // Get value from form element
+//       var zipNum = event.target.text.value;
+//       console.log(zipNum);
+//
+//       Meteor.subscribe("eventData", zipNum);
+//
+//       // Clear form
+//       event.target.text.value = "";
+//     }
+//   });
